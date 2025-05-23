97f2327b6caa387a3b0a0dd3d07fc89929171eb3
Kim Hyemi <qnlqlaqkq120@gmail.com>
2025-05-24 04:25:17 +0900

    test: EC2 GITHUB WOOKS IP 등록 후 테스트

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,6 @@ app.listen(PORT, () => {
   console.log("🚀 Jenkins 자동 배포 테스트용 로그!");
   console.log("🚀 Jenkins 인바운드 규칙 변경 후 테스트!");
   console.log("🚀 Jenkins ENDSSH 수정 후 테스트");
+  console.log("🚀 EC2 GITHUB WOOKS IP 등록 후 테스트");
 
 });
