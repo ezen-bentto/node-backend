bf34e26164d15016ed6abc7af92143545b7d7c60
Kim Hyemi <qnlqlaqkq120@gmail.com>
2025-05-24 03:29:13 +0900

    test: jenkins END SSH 수정 후 테스트

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,6 @@ app.listen(PORT, () => {
   console.log(`Server is listening on port : ${PORT}`);
   console.log("🚀 Jenkins 자동 배포 테스트용 로그!");
   console.log("🚀 Jenkins 인바운드 규칙 변경 후 테스트!");
+  console.log("🚀 Jenkins ENDSSH 수정 후 테스트");
+
 });
