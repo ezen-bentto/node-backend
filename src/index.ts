84d09b48882d055c5f6dc5d7443008bc3fedac1a
Kim Hyemi <qnlqlaqkq120@gmail.com>
2025-05-24 03:17:07 +0900

    test: jenkins 배포 테스트 로그 추가2

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,5 @@ const PORT = process.env.POST || 4000;
 app.listen(PORT, () => {
   console.log(`Server is listening on port : ${PORT}`);
   console.log("🚀 Jenkins 자동 배포 테스트용 로그!");
+  console.log("🚀 Jenkins 인바운드 규칙 변경 후 테스트!");
 });
