4ea7db7b145be5da12bbd9fedcdb10a53ec4585f
lugia574 <66871221+lugia574@users.noreply.github.com>
2025-05-27 20:35:26 +0900

    feat : getController 정상화화

--- src/controllers/demo/get.controller.ts
+++ src/controllers/demo/get.controller.ts
@@ -1,5 +1,4 @@
 import { ERROR_CODES } from '@/constants/error.constant';
-import { INTERNAL_SERVER_ERROR } from '@/constants/message.constant';
 import { DemoResponseSchema } from '@/schemas/demo.schema';
 import { DemoService } from '@/service/demo.service';
 import { AppError } from '@/utils/AppError';
@@ -7,17 +6,15 @@ import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { z } from 'zod';
 
-import { client } from '@/config/redis.config'
-
 export const get: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    await client.v4.set('key', 'test');
-    const value = await client.v4.get('key');
+    const data = await DemoService.get();
+    const parsed = z.array(DemoResponseSchema).safeParse(data);
+    if (!parsed.success) {
+      throw new AppError(StatusCodes.INTERNAL_SERVER_ERROR, ERROR_CODES.INTERNAL_ERROR);
+    }
 
-    res.status(200).json({
-      message: 'Value stored in Redis',
-      value,
-    });
+    res.status(StatusCodes.OK).json({ data: parsed.data });
   } catch (err) {
     next(err);
   }
