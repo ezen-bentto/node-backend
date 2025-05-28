49b98312697857b93336411c827c9fee13a10acc
YUL554 <hyr0197@gmail.com>
2025-05-29 00:08:34 +0900

    공모전 주석 적용용

--- src/controllers/contest/create.controller.ts
+++ src/controllers/contest/create.controller.ts
@@ -5,6 +5,15 @@ import { AppError } from '@/utils/AppError';
 import { Request, Response, NextFunction, RequestHandler } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
+/**
+ * ```
+ * 패키지명: controllers/contest
+ * 상세설명: 공모전 등록 컨트롤러
+ * ```
+ *
+ * @date 2025/05/29
+ * @author 한유리
+ */
 export const create: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
   const parsed = ContestCreateSchema.safeParse(req.body);
 
