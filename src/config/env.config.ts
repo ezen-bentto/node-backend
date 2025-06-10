4de330b00b8ffcd4958cf80b67ffd0509bd2d6e5
YUL554 <hyr0197@gmail.com>
2025-06-10 14:42:58 +0900

    fix : 레디스, timezon 오류 해결

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -12,7 +12,7 @@ export const ENV = {
     user: process.env.DB_USER as string,
     password: process.env.DB_PASSWORD as string,
     database: process.env.DB_DATABASE as string,
-    timezone: 'Asia/Seoul',
+    // timezone: 'Asia/Seoul',
   },
 
   jwt: {
