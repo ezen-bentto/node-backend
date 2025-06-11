49aa44ebca6578fae683647ef62f0412f05c4fc7
lugia574 <dlcjfdnr789@naver.com>
2025-06-11 15:00:07 +0900

    test : test

--- src/utils/common/trackViewByIp.ts
+++ src/utils/common/trackViewByIp.ts
@@ -1,7 +1,11 @@
 import { client } from '@/config/redis.config';
-import { ContestModel } from '@/models/contest.model';
 
-const trackViewByIp = async (resource: string, id: number, ip: string, contestView:string): Promise<void> => {
+const trackViewByIp = async (
+  resource: string,
+  id: number,
+  ip: string,
+  contestView: string
+): Promise<void> => {
   const ipKey = `${resource}${id}:ip${ip}`;
   const viewsKey = `${resource}:${id}:views`;
 
@@ -9,13 +13,13 @@ const trackViewByIp = async (resource: string, id: number, ip: string, contestVi
 
   // 이전에 들어왔다면 기록 X
   if (isVisited) return;
-  
+
   // 처음 들어오면 24시간 동안 IP 기록
   await client.v4.setEx(ipKey, 60 * 60 * 24, '1');
 
   // redis 조회수 count
-  const currentViews = await client.v4.get(viewsKey)
-  if(currentViews === null ){
+  const currentViews = await client.v4.get(viewsKey);
+  if (currentViews === null) {
     await client.v4.set(viewsKey, (parseInt(contestView) + 1).toString());
   } else {
     await client.v4.incr(viewsKey);
