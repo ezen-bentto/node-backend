36cbb94124e7deb965e26b1cc7da0f2218ec40f3
YoonSally <sdfg1001@naver.com>
2025-06-24 05:48:48 +0900

    fix : 카카오 소셜로그인 리프레시토큰 저장 오류 수정진행중

--- src/types/auth.type.ts
+++ src/types/auth.type.ts
@@ -1,7 +1,7 @@
 // src/types/auth.type.ts
 
 export interface KakaoUserInfo {
-  id: number;
+  id: number | bigint;
   kakao_account: {
     email?: string;
     profile?: {
@@ -21,21 +21,21 @@ export interface SocialUser {
 
 // --- DB에서 조회되는 사용자 정보의 실제 형태 ---
 export interface User {
-  user_id: number;
+  user_id: number | bigint;
   login_id: string;
   email?: string;
   password?: string;
   nickname: string;
   profile_image?: string;
-  user_type: string;
+  user_type: '개인' | '기업' | '관리자';
   approval_status?: string;
-  provider?: string;
+  provider?: '1' | '2' | '3' | '4' | null;
   // created_at, updated_at 등 다른 DB 컬럼이 있다면 여기에 추가
 }
 
 // JWT payload 및 req.user에 사용될 사용자 정보 (DB의 User 테이블과 매핑)
 export interface AuthUser {
-  id: number; // user_id (PK)
+  id: string; // user_id (PK)
   loginId: string; // login_id (필수: 소셜ID 또는 이메일)
   email?: string; // user.email 필드 (소셜로그인 이메일 또는 별도 저장 이메일)
   nickname: string;
