412588494b730e0989b7a40282b25c686c56dcb5
YoonSally <sdfg1001@naver.com>
2025-06-27 00:25:25 +0900

    feat : 북마크 리스트 데이터 쿼리 및 export 수정

--- src/models/mypage/mypage.model.ts
+++ src/models/mypage/mypage.model.ts
@@ -9,11 +9,14 @@ export class MypageModel {
     try {
       conn = await getDBConnection().getConnection();
       const query = `
-        SELECT community_id as id, title, content, reg_date as createdDate, community_type as communityType 
-        FROM community 
-        WHERE author_id = ? AND del_yn = 'N' 
-        ORDER BY reg_date DESC;
-      `;
+      SELECT 
+        community_id as id, title, content, reg_date as createdDate, 
+        community_type as communityType, category_type as categoryType, 
+        recruit_end_date as recruitEndDate
+      FROM community 
+      WHERE author_id = ? AND del_yn = 'N' 
+      ORDER BY reg_date DESC;
+    `;
       return await conn.query(query, [userId]);
     } catch (error) {
       console.error('findPostsByUserId 실패:', error);
@@ -29,7 +32,9 @@ export class MypageModel {
     try {
       conn = await getDBConnection().getConnection();
       const query = `
-        SELECT c.contest_id as id, c.title, c.organizer, c.end_date as endDate
+        SELECT 
+          c.contest_id as id, c.title, c.organizer, c.end_date as endDate, 
+          c.participant, c.prize, c.benifits
         FROM scrap s
         JOIN contest c ON s.target_id = c.contest_id
         WHERE s.user_id = ? AND s.target_type = '1' AND s.del_yn = 'N'
@@ -50,12 +55,17 @@ export class MypageModel {
     try {
       conn = await getDBConnection().getConnection();
       const query = `
-        SELECT co.community_id as id, co.title, co.content, co.reg_date as createdDate, co.community_type as communityType
-        FROM scrap s
-        JOIN community co ON s.target_id = co.community_id
-        WHERE s.user_id = ? AND s.target_type = '2' AND s.del_yn = 'N'
-        ORDER BY s.reg_date DESC;
-      `;
+      SELECT 
+        co.community_id as id, co.title, co.content, co.reg_date as createdDate, 
+        co.community_type as communityType, co.category_type as categoryType, 
+        co.recruit_end_date as recruitEndDate,
+        u.nickname as authorNickname 
+      FROM scrap s
+      JOIN community co ON s.target_id = co.community_id
+      JOIN user u ON co.author_id = u.user_id
+      WHERE s.user_id = ? AND s.target_type = '2' AND s.del_yn = 'N'
+      ORDER BY s.reg_date DESC;
+    `;
       return await conn.query(query, [userId]);
     } catch (error) {
       console.error('findBookmarkedCommunitiesByUserId 실패:', error);
