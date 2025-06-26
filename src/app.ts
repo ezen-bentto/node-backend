8cc966bdac57b253ab1b50e7e8f46f6164c65446
YoonSally <sdfg1001@naver.com>
2025-06-26 20:39:37 +0900

    fix : 마이페이지 500 오류 해결

--- src/app.ts
+++ src/app.ts
@@ -28,16 +28,16 @@ app.use('/api/common', CommonRouter);
 app.use('/api/scrap', ScrapRouter);
 app.use('/api/mypage', mypageRouter);
 
-// // 레디스 값 DB에 삽입
-// setInterval(
-//   () => {
-//     console.log('조회수 동기화 시작');
-//     syncViewsToDb('contest');
-//     // syncViewsToDb("community");
-//     // syncViewsToDb("policy");
-//   },
-//   1000 * 60 * 1
-// );
+// 레디스 값 DB에 삽입
+setInterval(
+  () => {
+    console.log('조회수 동기화 시작');
+    syncViewsToDb('contest');
+    // syncViewsToDb("community");
+    // syncViewsToDb("policy");
+  },
+  1000 * 60 * 1
+);
 
 // ✅ 3. 에러 핸들러 등록
 app.use(errorHandler);
--- src/controllers/mypage/mypage.controller.ts
+++ src/controllers/mypage/mypage.controller.ts
@@ -2,20 +2,21 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { MypageModel } from '../../models/mypage/mypage.model';
+import { serializeBigInt } from '../../utils/common/serializeBigInt';
 
 export class MypageController {
   private mypageModel = new MypageModel();
 
   // 프로필 정보는 authMiddleware에서 이미 처리했으므로 그대로 사용
   public getProfile = (req: Request, res: Response) => {
-    res.status(StatusCodes.OK).json({ success: true, data: req.user });
+    res.status(StatusCodes.OK).json({ success: true, data: serializeBigInt(req.user)});
   };
 
   public getMyPosts = async (req: Request, res: Response) => {
     const userId = Number(req.user!.id);
     try {
       const posts = await this.mypageModel.findPostsByUserId(userId);
-      res.status(StatusCodes.OK).json({ success: true, data: posts });
+      res.status(StatusCodes.OK).json({ success: true, data: serializeBigInt(posts)});
     } catch (error) {
       res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ success: false, message: '서버 오류' });
     }
@@ -25,7 +26,7 @@ export class MypageController {
     const userId = Number(req.user!.id);
     try {
       const contests = await this.mypageModel.findBookmarkedContestsByUserId(userId);
-      res.status(StatusCodes.OK).json({ success: true, data: contests });
+      res.status(StatusCodes.OK).json({ success: true, data: serializeBigInt(contests)});
     } catch (error) {
       res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ success: false, message: '서버 오류' });
     }
@@ -35,7 +36,7 @@ export class MypageController {
     const userId = Number(req.user!.id);
     try {
       const communities = await this.mypageModel.findBookmarkedCommunitiesByUserId(userId);
-      res.status(StatusCodes.OK).json({ success: true, data: communities });
+      res.status(StatusCodes.OK).json({ success: true, data: serializeBigInt(communities)});
     } catch (error) {
       res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ success: false, message: '서버 오류' });
     }
